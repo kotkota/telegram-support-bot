ee5f5e2e5a2d8db2cabeac938fe0d78250742f35
l1b3rtaS <86371143+l1b3rtaS@users.noreply.github.com>
2022-05-14 07:17:41 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,6 +17,6 @@ if TELEGRAM_SUPPORT_CHAT_ID is None or not str(TELEGRAM_SUPPORT_CHAT_ID).lstrip(
 TELEGRAM_SUPPORT_CHAT_ID = int(TELEGRAM_SUPPORT_CHAT_ID)
 
 
-WELCOME_MESSAGE = os.getenv("WELCOME_MESSAGE", "👋")
+WELCOME_MESSAGE = os.getenv("WELCOME_MESSAGE", "Здравстуй , это бот тех. поддержки , отправь вопрос и один из наших менеджеров на него ответит")
 REPLY_TO_THIS_MESSAGE = os.getenv("REPLY_TO_THIS_MESSAGE", "REPLY_TO_THIS")
 WRONG_REPLY = os.getenv("WRONG_REPLY", "WRONG_REPLY")
